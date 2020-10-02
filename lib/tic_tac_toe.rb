c2911d50679fbfdfaf0871781e5f9b4fb42a9de0
Nathan Rymarz <nrymarz@gmail.com>
2020-10-02 01:20:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,8 +5,11 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
+  win = WIN_COMBINATIONS.detect do |combo|
       position_taken?(board,combo[0]) && board[combo[0]] == board[combo[1]] && board[combo[0]]== board[combo[2]]
+  end
+  if win == nil
+    false 
   end
 end
 
